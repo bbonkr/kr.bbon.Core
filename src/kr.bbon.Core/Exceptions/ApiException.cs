c75fc1bab02ad7e18d9dc1eec1336483e571df4e
Pon Cheol Ku (구본철) <dev@bbon.kr>
2021-09-19 13:18:35 +0900

    Add ApiException class #7

---
+++ src/kr.bbon.Core/Exceptions/ApiException.cs
@@ -0,0 +1,54 @@
+﻿using kr.bbon.Core.Models;
+using System;
+using System.Net;
+
+namespace kr.bbon.Core
+{
+    /// <summary>
+    /// Represent situation that can not provide an appropriate response.
+    /// </summary>
+    public class ApiException : Exception
+    {
+        public const string Name = "ApiException ";
+
+        private const string DEFAULT_MESSAGE = "Could not provide an appropriate response.";
+
+        public ApiException(int statusCode, string message, ErrorModel error)
+            : base(message)
+        {
+            StatusCode = statusCode;
+            HttpStatusCode = (HttpStatusCode)statusCode;
+            Error = error ?? new ErrorModel
+            {
+                Code = $"{(HttpStatusCode)statusCode}",
+                Message = message,
+            };
+        }
+
+        public ApiException(HttpStatusCode statusCode, string message, ErrorModel error)
+            : this((int)statusCode, message, error) { }
+
+        public ApiException(int statusCode, ErrorModel error)
+            : this(statusCode, error.Message, error) { }
+
+        public ApiException(HttpStatusCode statusCode, ErrorModel error)
+            : this(statusCode, error.Message, error) { }
+
+        public ApiException(int statusCode, string message)
+            : this(statusCode, message, null) { }
+
+        public ApiException(HttpStatusCode statusCode, string message)
+            : this(statusCode, message, null) { }
+
+        public ApiException(int statusCode)
+            : this(statusCode, DEFAULT_MESSAGE) { }
+        public ApiException(HttpStatusCode statusCode)
+          : this(statusCode, DEFAULT_MESSAGE) { }
+
+        public int StatusCode { get; }
+
+        public HttpStatusCode HttpStatusCode { get; }
+
+        public ErrorModel Error { get; }
+    }
+}
--- src/kr.bbon.Core/Models/ErrorModel.cs
+++ src/kr.bbon.Core/Models/ErrorModel.cs
@@ -5,6 +5,15 @@ namespace kr.bbon.Core.Models
 {
     public class ErrorModel
     {
+        public ErrorModel() { }
+
+        public ErrorModel(string message, string code = null, IList<ErrorModel> innerErrors = null)
+        {
+            Message = message;
+            Code = code;
+            InnerErrors = innerErrors ?? new List<ErrorModel>();
+        }
+
         public string Code { get; set; }
 
         public string Message { get; set; }
