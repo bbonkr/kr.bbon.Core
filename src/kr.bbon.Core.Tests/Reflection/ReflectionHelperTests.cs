c8511660f9d468643620df4506f65426b858c417
Pon Cheol Ku (구본철) <dev@bbon.kr>
2021-10-11 16:05:50 +0900

    Fix stackoverflow exception
    - Update README.md

---
+++ src/kr.bbon.Core.Tests/Reflection/ReflectionHelperTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using kr.bbon.Core.Reflection;
+using Xunit;
+
+namespace kr.bbon.Core.Tests.Reflection
+{
+    public class ReflectionHelperTests
+    {
+        [Fact(DisplayName ="Should collect kr.bbon.Core")]
+        public void Collect_Assembly()
+        {
+            var assemblyName = "kr.bbon.Core";
+            var predicte = new Func<Type, bool>(t => t.Name == nameof(ApiException));
+
+            var assemblies = ReflectionHelper.CollectAssembly(predicte);
+
+            Assert.NotNull(assemblies);
+            
+            Assert.Contains(assemblies, a => a.GetName().Name == assemblyName);
+        }
+
+        [Fact(DisplayName ="Should collect ApiException type")]
+        public void Collect_Types()
+        {
+            var typeName = nameof(ApiException);
+            var predicte = new Func<Type, bool>(t => t.Name == typeName);
+
+            var types = ReflectionHelper.CollectTypes(predicte);
+
+            Assert.NotNull(types);
+
+            Assert.Contains(types, a => a.Name == typeName);
+        }
+
+        [Fact(DisplayName = "CollectTypes Should throw exception when predicate is null")]
+        public void CollectTypes_Should_Throws_When_Predicate_Is_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => {
+                Func<Type, bool> predicate = null;
+                foreach(var t in ReflectionHelper.CollectTypes(predicate))
+                {
+                    // nothing ;)
+                }
+            });
+        }
+
+        [Fact(DisplayName = "CollectAssemblty Shold throw exception when predicate is null")]
+        public void CollectAssemblty_Should_Throw_When_Predicate_Is_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => {
+                Func<Type, bool> predicate = null;
+                foreach (var t in ReflectionHelper.CollectAssembly(predicate))
+                {
+                    // nothing ;)
+                }
+            });
+        }
+    }
+}
