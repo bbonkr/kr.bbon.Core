24aa61d65ad0abfe8d862ee8ef0128e061f01e24
Pon Cheol Ku (구본철) <bbon@live.com>
2021-12-22 15:28:58 +0900

    Update ReflectionHelper class

--- src/kr.bbon.Core/Reflection/ReflectionHelper.cs
+++ src/kr.bbon.Core/Reflection/ReflectionHelper.cs
@@ -25,7 +25,7 @@ namespace kr.bbon.Core.Reflection
         /// <returns></returns>
         public static IEnumerable<Assembly> CollectAssembly(Func<Type, int, bool> predicate)
         {
-            return CollectAssembly(null,predicate);
+            return CollectAssembly(null, predicate);
         }
 
         private static IEnumerable<Assembly> CollectAssembly(Func<Type, bool> predicate = null, Func<Type, int, bool> predicateWithIndex = null)
@@ -62,7 +62,9 @@ namespace kr.bbon.Core.Reflection
         /// <returns></returns>
         public static IEnumerable<Type> CollectTypes(Func<Type, bool> predicate)
         {
-            return CollectTypes(predicate, null);
+            var allAssembly = AppDomain.CurrentDomain.GetAssemblies().Where(a => !a.IsDynamic);
+
+            return CollectTypes(allAssembly, predicate, null);
         }
 
         /// <summary>
@@ -72,21 +74,66 @@ namespace kr.bbon.Core.Reflection
         /// <returns></returns>
         public static IEnumerable<Type> CollectTypes(Func<Type, int, bool> predicate)
         {
-            return CollectTypes(null, predicate);
+            var allAssembly = AppDomain.CurrentDomain.GetAssemblies().Where(a => !a.IsDynamic);
+            return CollectTypes(allAssembly, null, predicate);
         }
 
-        private static IEnumerable<Type> CollectTypes(Func<Type, bool> predicate = null, Func<Type, int, bool> predicateWithIndex = null)
+        /// <summary>
+        /// Collect list of types that satisfy the condition are collected
+        /// </summary>
+        /// <param name="assemblies"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IEnumerable<Type> CollectTypesFrom(IEnumerable<Assembly> assemblies, Func<Type, int, bool> predicate)
+        {
+            return CollectTypes(assemblies, null, predicate);
+        }
+
+        /// <summary>
+        /// Collect list of types that satisfy the condition are collected
+        /// </summary>
+        /// <param name="assemblies"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IEnumerable<Type> CollectTypesFrom(IEnumerable<Assembly> assemblies, Func<Type, bool> predicate)
+        {
+            return CollectTypes(assemblies, predicate, null);
+        }
+
+        /// <summary>
+        /// Collect list of types that satisfy the condition are collected
+        /// </summary>
+        /// <param name="assemblies"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IEnumerable<Type> CollectTypesFrom(Assembly assembly, Func<Type, int, bool> predicate)
+        {
+            var assemblies = new List<Assembly> { assembly };
+            return CollectTypes(assemblies, null, predicate);
+        }
+
+        /// <summary>
+        /// Collect list of types that satisfy the condition are collected
+        /// </summary>
+        /// <param name="assemblies"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IEnumerable<Type> CollectTypesFrom(Assembly assembly, Func<Type, bool> predicate)
+        {
+            var assemblies = new List<Assembly> { assembly };
+            return CollectTypes(assemblies, predicate, null);
+        }
+
+        private static IEnumerable<Type> CollectTypes(IEnumerable<Assembly> assemblies, Func<Type, bool> predicate = null, Func<Type, int, bool> predicateWithIndex = null)
         {
             if (predicateWithIndex == null && predicate == null)
             {
                 throw new ArgumentNullException("Predicate must be set. Predicate should describe what type includes.");
             }
 
-            var allAssembly = AppDomain.CurrentDomain.GetAssemblies().Where(a => !a.IsDynamic);
-
             var condition = predicateWithIndex ?? new Func<Type, int, bool>((t, i) => predicate(t));
 
-            foreach (var assembly in allAssembly)
+            foreach (var assembly in assemblies)
             {
                 var foundTypes = assembly
                     .GetExportedTypes()
