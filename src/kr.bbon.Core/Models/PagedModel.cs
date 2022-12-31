b7eb84e3da9ce28015ac13f21c96a2a4cd1ced1d
Pon Cheol Ku (구본철) <bbon@live.com>
2022-12-31 14:13:46 +0900

    feat: PagedModel.Items #62

--- src/kr.bbon.Core/Models/PagedModel.cs
+++ src/kr.bbon.Core/Models/PagedModel.cs
@@ -3,150 +3,66 @@ using System.Linq;
 
 namespace kr.bbon.Core.Models
 {
-    public interface IPagedModel<TModel> where TModel : class
-    {
-        /// <summary>
-        /// Current page
-        /// </summary>
-        int CurrentPage { get; set; }
-
-        /// <summary>
-        /// Records
-        /// </summary>
-        List<TModel> Items { get; }
-
-        /// <summary>
-        /// Items count per page
-        /// </summary>
-        int Limit { get; set; }
-
-        /// <summary>
-        /// Total items count
-        /// </summary>
-        int TotalItems { get; set; }
-
-        /// <summary>
-        /// Total page count
-        /// </summary>
-        int TotalPages { get; set; }
-
-        /// <summary>
-        /// Has next page
-        /// </summary>
-        bool HasNextPage { get; set; }
-
-        /// <summary>
-        /// has previous page
-        /// </summary>
-        bool HasPreviousPage { get; set; }
-
-        /// <summary>
-        /// Current page is first page or not
-        /// </summary>
-        bool IsFirstPage { get; set; }
-
-        /// <summary>
-        /// Current page is last page or not
-        /// </summary>
-        bool IsLastPage { get; set; }
-    }
-
-
     public class PagedModel<TModel> : IPagedModel<TModel> where TModel : class
     {
-        //#if NET5_0_OR_GREATER
-        //        public PagedModel() { }
-        //#else
-        //        private PagedModel()
-        //        {
-        //        }
-        //#endif
-
         public PagedModel() { }
 
-        public PagedModel(int currentPage, int limit, int totalItems, int totalPages, IEnumerable<TModel> items)
+        public PagedModel(uint currentPage, uint limit, ulong totalItems, uint totalPages, IEnumerable<TModel> items)
         {
-            CurrentPage = currentPage;
-            Limit = limit;
-            TotalItems = totalItems;
-            TotalPages = totalPages;
-            Items = new List<TModel>(items ?? Enumerable.Empty<TModel>());
-
-            HasNextPage = CurrentPage < TotalPages;
-            HasPreviousPage = CurrentPage > 1;
-
-            IsFirstPage = currentPage == 1;
-            IsLastPage = currentPage == TotalPages;
+            SetInformation(currentPage, limit, totalItems, totalPages);
+            SetItems(items);
         }
 
         /// <inheritdoc />
-        public int CurrentPage
-        {
-            get;
-            //#if NET5_0_OR_GREATER
-            //            init;
-            //#endif
-            set;
-        }
+        public uint CurrentPage { get; private set; }
 
         /// <inheritdoc />
-        public int Limit
-        {
-            get;
-            //#if NET5_0_OR_GREATER
-            //            init;
-            //#endif
-            set;
-        }
+        public uint Limit { get; private set; }
 
         /// <inheritdoc />
-        public int TotalItems
-        {
-            get;
-            //#if NET5_0_OR_GREATER
-            //            init;
-            //#endif
-            set;
-        }
+        public ulong TotalItems { get; private set; }
 
         /// <inheritdoc />
-        public int TotalPages
-        {
-            get;
-            //#if NET5_0_OR_GREATER
-            //            init;
-            //#endif
-            set;
-        }
+        public uint TotalPages { get; private set; }
 
         /// <inheritdoc />
-        public List<TModel> Items
-        {
-            get;
-            //#if NET5_0_OR_GREATER
-            //            init;
-            //#endif
-            set;
-        }
+        public List<TModel> Items { get; private set; }
 
         /// <inheritdoc />
         public bool HasNextPage
         {
-            get;// => CurrentPage < TotalPages;
-            set;
+            get => CurrentPage < TotalPages;
         }
 
         /// <inheritdoc />
         public bool HasPreviousPage
         {
-            get;// => CurrentPage > 1;
-            set;
+            get => CurrentPage > 1;
         }
 
         /// <inheritdoc />
-        public bool IsFirstPage { get; set; }
+        public bool IsFirstPage
+        {
+            get => CurrentPage == 1;
+        }
 
         /// <inheritdoc />
-        public bool IsLastPage { get; set; }
+        public bool IsLastPage
+        {
+            get => CurrentPage == TotalPages;
+        }
+
+        public void SetInformation(uint currentPage, uint limit, ulong totalItems, uint totalPages)
+        {
+            CurrentPage = currentPage;
+            Limit = limit;
+            TotalItems = totalItems;
+            TotalPages = totalPages;
+        }
+
+        public void SetItems(IEnumerable<TModel> items)
+        {
+            Items = new List<TModel>(items ?? Enumerable.Empty<TModel>());
+        }
     }
 }
